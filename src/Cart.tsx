b3c73f139dc41e7fcb01b996a5982ce446e47939
AburizalAN <56324469+AburizalAN@users.noreply.github.com>
2023-11-07 22:23:06 +0700

    feature cart: create ui for cart

--- src/Cart.tsx
+++ src/Cart.tsx
@@ -1,4 +1,8 @@
+import * as React from "react";
 import styled from "@emotion/styled";
+import Modal from "./components/Modal";
+import { FaChevronDown } from "react-icons/fa";
+import { BsCartPlus } from "react-icons/bs";
 
 const ViewContainer = styled.div`
   max-width: 500px;
@@ -7,29 +11,43 @@ const ViewContainer = styled.div`
 `;
 
 const Navbar = styled.nav`
-  padding: 12px;
+  padding: 16px 12px;
   border-bottom: 1px solid #eaeaea;
+  display: flex;
+  align-items: center;
+
 `;
 
 const Content = styled.div`
-  padding-top: 16px;
+  padding: 16px 0;
 `
 
 const PageTitle = styled.div`
   font-weight: 700;
   color: #484848;
+  flex-grow: 1;
 `;
 
-const ProductItem = styled.div`
-  border: 1px solid #d9d9d9;
+const CartIcon = styled.div`
+  color: #484848;
+  padding: 4px;
+  border: 1px solid #c0c0c0;
+  border-radius: 4px;
+  width: 35px;
+  height: 35px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  &:hover {
+    background-color: #ececec;
+  }
+`
+
+const ProductItemWrapper = styled.div`
+  border: 1px solid #f1f1f1;
   border-radius: 8px;
   padding: 8px;
-`;
-
-const ProductsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  row-gap: 16px;
   .product {
     display: flex;
     .image {
@@ -42,13 +60,80 @@ const ProductsContainer = styled.div`
       margin-right: 16px;
     }
     .text {
-      .title {
-        
-      }
+      flex-grow: 1;
+      .title {}
       .price {
         font-weight: 700;
       }
+      .counter {
+        margin-top: 4px;
+        display: flex;
+        justify-content: right;
+        column-gap: 4px;
+        .btn-counter {
+          width: 20px;
+          height: 20px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          background: #7A9D54;
+          border: none;
+          border-radius: 4px;
+          color: white;
+        }
+        .amount {
+          width: 40px;
+          text-align: center;
+          border-radius: 4px;
+          border: 1px solid #e7e7e7;
+        }
+      }
     }
+  }
+`;
+
+const ProductsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 12px;
+`;
+
+const ProductGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 12px;
+  padding: 12px;
+  border: 1px solid #d9d9d9;
+  border-radius: 8px;
+`;
+
+const SelectShippingWrapper = styled.div`
+  padding: 8px;
+  border: 1px solid #f1f1f1;
+  border-radius: 8px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  .title {
+    flex-grow: 1;
+  }
+  .icon {
+    padding: 4px;
+    color: #484848;
+  }
+`;
+
+const ShippingList = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ShippingItem = styled.div`
+  padding: 8px;
+  border-radius: 4px;
+  cursor: pointer;
+  &:hover {
+    background-color: #f4f4f4;
   }
 `;
 
@@ -57,31 +142,84 @@ const Cart = () => {
     <ViewContainer>
       <Navbar>
         <PageTitle>Contoh Cart</PageTitle>
+        <CartIcon title="Tambah product"><BsCartPlus /></CartIcon>
       </Navbar>
       <Content>
         <ProductsContainer>
-          <ProductItem>
-            <div className="product">
-              <div className="image">
-
-              </div>
-              <div className="text">
-                <div className="title">
-                  Mobil Keren Punya Abu
-                </div>
-                <div className="price">
-                  {new Intl.NumberFormat("id-ID").format(1000)}
-                </div>
-              </div>
-            </div>
-          </ProductItem>
-          <ProductItem>
-            Ini Product Item;
-          </ProductItem>
+          <ProductGroup>
+            {[...Array(4)].map((_, i) => (
+              <ProductItem key={i} />
+            ))}
+            <SelectShipping />
+          </ProductGroup>
+          <ProductGroup>
+            {[...Array(4)].map((_, i) => (
+              <ProductItem key={i} />
+            ))}
+            <SelectShipping />
+          </ProductGroup>
         </ProductsContainer>
       </Content>
     </ViewContainer>
   )
 };
+
+const ProductItem = () => {
+  const [count, setCount] = React.useState(1);
+
+  return (
+    <ProductItemWrapper>
+      <div className="product">
+        <div className="image">
+
+        </div>
+        <div className="text">
+          <div className="title">
+            Mobil Keren Punya Abu
+          </div>
+          <div className="price">
+            {new Intl.NumberFormat("id-ID").format(1000)}
+          </div>
+          <div className="counter">
+            <button className="btn-counter" onClick={() => setCount(count > 1 ? count - 1 : 1)}>-</button>
+            <input disabled className="amount" value={count} />
+            <button className="btn-counter" onClick={() => setCount(count + 1)}>+</button>
+          </div>
+        </div>
+      </div>
+    </ProductItemWrapper>
+  )
+}
+
+const SelectShipping = () => {
+  const [ isOpen, setIsOpen ] = React.useState(false);
+  const [ selectedShipping, setSelectedShipping ] = React.useState(null);
+  return (
+    <>
+      <SelectShippingWrapper onClick={() => setIsOpen(true)}>
+        <div className="title">Select Shipping</div>
+        <div className="icon"><FaChevronDown /></div>
+      </SelectShippingWrapper>
+      <Modal
+        visible={isOpen}
+        onCancel={() => setIsOpen(false)}
+        centerPosition={true}
+        title="Select Shipping"
+      >
+        <ShippingList>
+          <ShippingItem>
+            test 1
+          </ShippingItem>
+          <ShippingItem>
+            test 1
+          </ShippingItem>
+          <ShippingItem>
+            test 1
+          </ShippingItem>
+        </ShippingList>
+      </Modal>
+    </>
+  )
+}
 
 export default Cart;
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,10 +1,203 @@
-const Modal = ({ onClose }: { onClose: () => void }) => {
-  return (
-    <div className="modal">
-      <div style={{ marginBottom: "24px" }}>Ini contoh modal</div>
-      <button onClick={onClose}>close</button>
-    </div>
-  )
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import clsx from "clsx";
+import { RiCloseFill } from "react-icons/ri";
+import styled from "@emotion/styled";
+
+type ModalProps = {
+  width?: number | string;
+  className?: string;
+  visible: boolean;
+  onCancel: () => void;
+  maskClosable?: boolean;
+  centerPosition?: boolean;
+  title?: string;
+  closeIcon?: boolean | null;
+};
+
+const ModalWrapper = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 99999;
+  display: flex;
+  justify-content: center;
+  animation: opacityIn 200ms forwards cubic-bezier(0, 0.5, 0.25, 1);
+  &.closing {
+    animation: opacityOut 200ms forwards cubic-bezier(0, 0.5, 0.25, 1);
+  }
+  & > .content {
+    margin: 100px auto auto;
+    padding: 24px;
+    border-radius: 12px;
+    background-color: white;
+    &.center {
+      margin: auto;
+    }
+    .title-wrapper {
+      position: relative;
+      .title {
+        font-weight: 700;
+        margin-bottom: 20px;
+      }
+      .close-icon {
+        position: absolute;
+        top: 0;
+        right: 0;
+        .button-close {
+          color: #7b7b7b;
+          padding: 4px;
+          border: 1px solid #b5b5b5;
+          border-radius: 4px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          cursor: pointer;
+        }
+      }
+    }
+  }
+  .modal {
+    animation: modalIn 200ms forwards cubic-bezier(0, 0.5, 0.25, 1);
+    z-index: 99999;
+    &.closing {
+      animation: modalOut 200ms forwards cubic-bezier(0, 0.5, 0.25, 1);
+    }
+  }
+  @keyframes modalIn {
+    0% {
+      opacity: 0;
+      transform: scale(0.95);
+    }
+    100% {
+      opacity: 1;
+      transform: scale(1);
+    }
+  }
+
+  @keyframes modalOut {
+    0% {
+      opacity: 1;
+      transform: scale(1);
+    }
+    100% {
+      opacity: 0;
+      transform: scale(0.95);
+    }
+  }
+
+  @keyframes opacityIn {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 100%;
+    }
+  }
+
+  @keyframes opacityOut {
+    0% {
+      opacity: 100%;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+`
+
+const ModalComp = ({
+  width = 500,
+  className,
+  children,
+  visible,
+  onCancel,
+  maskClosable = true,
+  centerPosition = false,
+  title,
+  closeIcon = true,
+}: React.PropsWithChildren<ModalProps>) => {
+  const modalRef = React.useRef<HTMLDivElement>(null);
+  const modalWrapperRef = React.useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    if (visible) {
+      setIsOpen(true);
+    } else {
+      const modal = modalRef.current;
+      const modalWrapper = modalWrapperRef.current;
+      if (!modal || !modalWrapper) return;
+      modal.classList.add("closing");
+      modalWrapper.classList.add("closing");
+      modal.addEventListener(
+        "animationend",
+        () => {
+          setIsOpen(false);
+        },
+        { once: true }
+      );
+    }
+  }, [visible]);
+
+  React.useEffect(() => {
+    const modal = modalRef.current;
+    if (!modal) return;
+  }, [modalRef.current]);
+
+  React.useEffect(() => {
+    if (maskClosable) {
+      document.addEventListener("mousedown", (event: Event) => {
+        if (
+          modalWrapperRef.current?.contains(event.target as Node) &&
+          !modalRef.current?.contains(event.target as Node)
+        ) {
+          onCancel();
+        }
+      });
+    }
+  }, [maskClosable]);
+
+  return isOpen ? (
+    <ModalWrapper
+      ref={modalWrapperRef}
+    >
+      <div
+        ref={modalRef}
+        className={clsx(
+          "content",
+          centerPosition ? "center" : null,
+          "modal",
+          className
+        )}
+        style={{ width }}
+      >
+        <div className="title-wrapper">
+          {title ? <h5 className="title">{title}</h5> : null}
+          {closeIcon ? (
+            <div className="close-icon">
+              <button onClick={onCancel} className="button-close">
+                <RiCloseFill />
+              </button>
+            </div>
+          ) : null}
+        </div>
+        <div>
+          {children}
+        </div>
+      </div>
+    </ModalWrapper>
+  ) : null;
+};
+
+const Modal = (props: React.PropsWithChildren<ModalProps>) => {
+  const renderComponent = (
+    <ModalComp {...props} />
+  );
+
+  return ReactDOM.createPortal(renderComponent, document.body);
 }
 
 export default Modal;
